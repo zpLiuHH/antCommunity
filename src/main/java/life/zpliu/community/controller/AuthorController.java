b232b5c20a86786e94e6d428d03c85836a991c8e
zpLiu <liuzhipeng1302@163.com>
2019-08-02 18:35:42 +0800

    getAcessToken  getUser githubDTO

--- src/main/java/life/zpliu/community/controller/AuthorController.java
+++ src/main/java/life/zpliu/community/controller/AuthorController.java
@@ -1,9 +1,15 @@
 package life.zpliu.community.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import life.zpliu.community.dto.AccessTokenDTO;
+import life.zpliu.community.provider.GitHubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.URI;
 
 /**
  * @author zpliu
@@ -11,8 +17,21 @@ import org.springframework.web.bind.annotation.RequestParam;
  */
 @Controller
 public class AuthorController {
+    @Autowired
+    private GitHubProvider gitHubProvider;
+
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam( name="state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
+        String client_id = "c65a2086684ea6420af8";
+        String client_secret = "2197fdcbeee14e2d71f2b5f2cbe94c29b4377048";
+        String redirect_uri = "http://localhost:8887/callback";
+        AccessTokenDTO accessTokenDto = new AccessTokenDTO();
+        accessTokenDto.setCode(code);
+        accessTokenDto.setState(state);
+        accessTokenDto.setClient_id(client_id);
+        accessTokenDto.setClient_secret(client_secret);
+        accessTokenDto.setRedirect_uri(redirect_uri);
+        gitHubProvider.getAccessToken(accessTokenDto);
         return "index";
     }
 
--- src/main/java/life/zpliu/community/provider/GitHubProvider.java
+++ src/main/java/life/zpliu/community/provider/GitHubProvider.java
@@ -1,7 +1,12 @@
 package life.zpliu.community.provider;
 
+import com.alibaba.fastjson.JSON;
 import life.zpliu.community.dto.AccessTokenDTO;
+import life.zpliu.community.dto.GitHubUser;
+import okhttp3.*;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 /**
  * @author: liuzhipeng
@@ -9,8 +14,40 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class GitHubProvider {
-    public String getAccessToken(AccessTokenDTO accessTokenDto){
+    public String getAccessToken(AccessTokenDTO accessTokenDto) {
+        MediaType mediaType = MediaType.get("application/json; charset=utf-8");
+        String url = "https://github.com/login/oauth/access_token";
+        OkHttpClient client = new OkHttpClient();
+        RequestBody body = RequestBody.create(mediaType, JSON.toJSONString(accessTokenDto));
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            System.out.print(response.body().string());
+            String callBackStr = response.body().string();
+            String[] split = callBackStr.split("&");
+            String access_token = split[0].split("=")[1];
+            return access_token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
+    public GitHubUser getUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        String url = "https://api.github.com/user?access_token";
+        Request request = new Request.Builder()
+                .url(url + accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            System.out.println(response.body().string());
+            GitHubUser user = JSON.parseObject(response.body().string(), GitHubUser.class);
+            return user;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 }
